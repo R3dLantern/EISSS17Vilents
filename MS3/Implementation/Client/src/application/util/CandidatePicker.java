08042452ddd7919e2608edba742de20fac724555
Leon <lvilents@gmail.com>
2017-07-06 18:04:16 +0200

    Kandidaten-Picker-Logik vollständig!

--- MS3/Implementation/Client/src/application/util/CandidatePicker.java
+++ MS3/Implementation/Client/src/application/util/CandidatePicker.java
@@ -4,6 +4,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import application.Main;
+
 /**
  * Berechnet ElementRankings von Benutzerlisten und errechnet so die bestmöglichen Kandidaten für die Top-Liste
  * @author Leonid Vilents
@@ -18,17 +20,63 @@ public class CandidatePicker {
 	 */
 	public JSONArray getCandidatesForTopChoice(JSONArray applicants)
 	{
-		// Nach Gesamtreputation sortieren
-		QuickSorter.sort(applicants);
-		JSONArray arrayWithRankings = this.calculateRankings(this.getUpperQuartile(applicants));
-		return arrayWithRankings;
+		try {
+			// Nach Gesamtreputation sortieren
+			QuickSorter.sort(applicants);
+			// Es wird nur das obere Quartil an Daten betrachtet
+			JSONArray arrayWithRankings = this.calculateRankings(this.getUpperQuartile(applicants));
+			return getTopCandidatesBasedOnRankings(arrayWithRankings);
+		} catch (JSONException je) {
+			je.printStackTrace();
+			return null;
+		}
 	}
 	
 	//TODO: Kandidaten basierend auf Ranking auswählen
-	/*private JSONArray setTopCandidatesBasedOnRankings(JSONArray array)
+	private JSONArray getTopCandidatesBasedOnRankings(JSONArray array) throws JSONException
 	{
+		Main.log(array.toString());
+		// 3 Topkandidaten
+		// 0: ProjektRanking Bester
+		// 1: Projekt-Update-Kombintation Bester
+		// 2: Kommentare Bester
+		JSONArray topCandidates = new JSONArray();
 		
-	}*/
+		int lastIndex = array.length() - 1;
+		// Sortiere das übergebene Array nach verschiedenen Rankings
+		JSONArray sortedByHighestProjectUpdatesRanking = BubbleSorter.sort(array, "projectUpdatesRanking");
+		JSONObject topCandidateProjectUpdates = sortedByHighestProjectUpdatesRanking.getJSONObject(lastIndex);
+		topCandidates.put(topCandidateProjectUpdates);
+		
+		JSONArray sortedByHighestProjectsRanking = array;
+		for(int i = 0; i < sortedByHighestProjectsRanking.length(); i++) {
+			if(sortedByHighestProjectsRanking.getJSONObject(i).getInt("id") == topCandidateProjectUpdates.getInt("id")) {
+				sortedByHighestProjectsRanking.remove(i);
+				i = sortedByHighestProjectsRanking.length();
+			}
+		}
+		
+		lastIndex = sortedByHighestProjectsRanking.length() - 1;
+		sortedByHighestProjectsRanking = BubbleSorter.sort(sortedByHighestProjectsRanking, "projectRanking");
+		JSONObject topCandidateProjects = sortedByHighestProjectsRanking.getJSONObject(lastIndex);
+		topCandidates.put(topCandidateProjects);
+		
+		
+		JSONArray sortedByHighestCommentsRanking = sortedByHighestProjectsRanking;
+		for(int i = 0; i < sortedByHighestCommentsRanking.length(); i++) {
+			if(sortedByHighestCommentsRanking.getJSONObject(i).getInt("id") == topCandidateProjects.getInt("id")) {
+				sortedByHighestCommentsRanking.remove(i);
+				i = sortedByHighestCommentsRanking.length();
+			}
+		}
+		
+		lastIndex = sortedByHighestCommentsRanking.length() - 1;
+		sortedByHighestCommentsRanking = BubbleSorter.sort(sortedByHighestCommentsRanking, "commentsRanking");
+		JSONObject topCandidateComments = sortedByHighestCommentsRanking.getJSONObject(lastIndex);
+		topCandidates.put(topCandidateComments);
+		
+		return topCandidates;
+	}
 	
 	
 	/**
@@ -37,42 +85,37 @@ public class CandidatePicker {
 	 * @param array
 	 * @return JSONArray mit erweiterten Benutzer-JSONObjects.
 	 */
-	private JSONArray calculateRankings (JSONArray array)
+	private JSONArray calculateRankings (JSONArray array) throws JSONException
 	{
 		JSONArray arrayWithRankings = new JSONArray();
 		
 		for (int i = 0; i < array.length(); i++) {
-			try {
-				JSONObject userObject = array.getJSONObject(i);
-				JSONObject counters = userObject.getJSONObject("counters");
+			JSONObject userObject = array.getJSONObject(i);
+			JSONObject counters = userObject.getJSONObject("counters");
 				
-				JSONObject rankings = new JSONObject();
-				// Ranking für Projekte
-				int projectCount = counters.getInt("projects");
-				int projectUpvotesCount = counters.getInt("projectUpvotes");
-				int projectRanking = (int) Math.floor((projectCount + projectUpvotesCount) /2);
-				rankings.put("projectRanking", projectRanking);
+			JSONObject rankings = new JSONObject();
+			// Ranking für Projekte
+			int projectCount = counters.getInt("projects");
+			int projectUpvotesCount = counters.getInt("projectUpvotes");
+			int projectRanking = (int) Math.floor((projectCount + projectUpvotesCount) /2);
+			rankings.put("projectRanking", projectRanking);
 				
-				// Ranking für Projekt-Update-Kombinationen
-				int projectUpdatesCount = counters.getInt("projectUpdates");
-				int projectUpdateUpvotesCount = counters.getInt("projectUpdateUpvotes");
-				int partialRanking = (int) Math.floor((projectUpdatesCount + projectUpdateUpvotesCount) /2);
-				int projectUpdatesRanking = (int) Math.floor((projectRanking + partialRanking) / 2);
-				rankings.put("projectUpdatesRanking", projectUpdatesRanking);
+			// Ranking für Projekt-Update-Kombinationen
+			int projectUpdatesCount = counters.getInt("projectUpdates");
+			int projectUpdateUpvotesCount = counters.getInt("projectUpdateUpvotes");
+			int partialRanking = (int) Math.floor((projectUpdatesCount + projectUpdateUpvotesCount) /2);
+			int projectUpdatesRanking = (int) Math.floor((projectRanking + partialRanking) / 2);
+			rankings.put("projectUpdatesRanking", projectUpdatesRanking);
+			
+			// Ranking für Kommentare
+			int commentsCount = counters.getInt("comments");
+			int commentUpvotesCount = counters.getInt("commentUpvotes");
+			int commentsRanking = (int) Math.floor((commentsCount + commentUpvotesCount) / 2);
+			rankings.put("commentsRanking", commentsRanking);
 				
-				// Ranking für Kommentare
-				int commentsCount = counters.getInt("comments");
-				int commentUpvotesCount = counters.getInt("commentUpvotes");
-				int commentsRanking = (int) Math.floor((commentsCount + commentUpvotesCount) / 2);
-				rankings.put("commentsRanking", commentsRanking);
+			userObject.put("rankings", rankings);
 				
-				userObject.put("rankings", rankings);
-				
-				arrayWithRankings.put(userObject);
-			} catch (JSONException je) {
-				je.printStackTrace();
-				return null;
-			}
+			arrayWithRankings.put(userObject);
 		}
 		return arrayWithRankings;
 	}
@@ -82,7 +125,7 @@ public class CandidatePicker {
 	 * @param array JSON-Array mit Benutzern
 	 * @return JSONArray mit oberem Quartil des Parameter-Arrays
 	 */
-	private JSONArray getUpperQuartile(JSONArray array)
+	private JSONArray getUpperQuartile(JSONArray array) throws JSONException
 	{
 		int n = array.length();
 		int q3 = (int) (0.75 * (n+1));
@@ -91,29 +134,23 @@ public class CandidatePicker {
 			return array;
 		}
 		
-		try {
-			JSONArray q3array = new JSONArray();
-			for (int i = q3 - 1; i < n; i++) {
-				q3array.put(array.getJSONObject(i));
-			}
+		JSONArray q3array = new JSONArray();
+		for (int i = q3 - 1; i < n; i++) {
+			q3array.put(array.getJSONObject(i));
+		}
 			
-			// Arrays kleiner 6 werden aufgefüllt
-			if(q3array.length() < 6) {
-				int j = q3-1;
-				while(q3array.length() != 3 ){
-					q3array.put(array.getJSONObject(--j));
-				}
-				return q3array;
-
-			//Von größeren Arrays wird ein weiteres Oberes Quartil angefordert
-			} else if (q3array.length() > 18) {	
-				return getUpperQuartile(q3array);
-			} else {
-				return q3array;
+		// Arrays kleiner 6 werden aufgefüllt
+		if(q3array.length() < 6) {
+			int j = q3-1;
+			while(q3array.length() != 3 ){
+				q3array.put(array.getJSONObject(--j));
 			}
-		} catch(JSONException je) {
-			je.printStackTrace();
-			return null;
+			return q3array;
+			//Von größeren Arrays wird ein weiteres Oberes Quartil angefordert
+		} else if (q3array.length() > 18) {	
+			return getUpperQuartile(q3array);
+		} else {
+			return q3array;
 		}
 	}
 }
